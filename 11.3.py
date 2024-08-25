a9116feab036a837eec9f7c777c5e9cf6347d5ac
Oleg <sagdeev.ol@yandex.ru>
2024-08-25 21:49:43 +0300

    Add files via upload

---
+++ 11.3.py
@@ -0,0 +1,21 @@
+def introspection_info(obj):
+    info = {
+        'type': type(obj).__name__, 'attributes': dir(obj),
+        'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
+        'module': getattr(obj, '__module__', None)
+    }
+    return info
+
+number_info = introspection_info(42)
+print(number_info)
+
+class Primer:
+    def __init__(self, value):
+        self.value = value
+
+    def my_method(self):
+        return self.value
+
+my_object = Primer(10)
+object_info = introspection_info(my_object)
+print(object_info)
