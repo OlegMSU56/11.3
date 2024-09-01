65098d9244129a9dd235a36361dbbc9ff738861a
Oleg <sagdeev.ol@yandex.ru>
2024-09-01 23:06:16 +0300

    Update 11.3.py

--- 11.3.py
+++ 11.3.py
@@ -1,6 +1,6 @@
 def introspection_info(obj):
     info = {
-        'type': type(obj).__name__, 'attributes': dir(obj),
+        'type': type(obj).__name__, 'attributes': [attr for attr in dir(obj) if not callable(getattr(obj, attr))],
         'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
         'module': getattr(obj, '__module__', None)
     }
